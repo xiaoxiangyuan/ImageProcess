d78cc71746b7d28846106b33b04b8a76e4faa669
Danseling <1779764587@qq.com>
2019-04-05 21:20:06 +0800

    build a qt project

--- Image_Process/Image_Process/Image_Process.h
+++ Image_Process/Image_Process/Image_Process.h
@@ -1,32 +1,15 @@
-
-// Image_Process.h : PROJECT_NAME 应用程序的主头文件
-//
-
 #pragma once
 
-#ifndef __AFXWIN_H__
-	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
-#endif
+#include <QtWidgets/QMainWindow>
+#include "ui_Image_Process.h"
 
-#include "resource.h"		// 主符号
-
-
-// CImage_ProcessApp: 
-// 有关此类的实现，请参阅 Image_Process.cpp
-//
-
-class CImage_ProcessApp : public CWinApp
+class Image_Process : public QMainWindow
 {
+	Q_OBJECT
+
 public:
-	CImage_ProcessApp();
+	Image_Process(QWidget *parent = Q_NULLPTR);
 
-// 重写
-public:
-	virtual BOOL InitInstance();
-
-// 实现
-
-	DECLARE_MESSAGE_MAP()
+private:
+	Ui::Image_ProcessClass ui;
 };
-
-extern CImage_ProcessApp theApp;
